6be7608238d587ca1e9281936ccad32df4b80c57
MikleBoroda <mikle.kuznecov@gmail.com>
2021-05-15 12:51:20 +0300

    Реализация методов спомощью двух клаасов Calculator и Application

---
+++ src/main/java/lesson3/task1/Application.java
@@ -0,0 +1,13 @@
+package lesson3.task1;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.summ(2, 4));
+        System.out.println(calculator.substract(5, 3));
+        System.out.println(calculator.multiply(2, 6));
+        System.out.println(calculator.divide(5, 3));
+
+    }
+}
---
+++ src/main/java/lesson3/task1/Calculator.java
@@ -0,0 +1,21 @@
+package lesson3.task1;
+
+public class Calculator {
+
+    int summ(int term, int composer) {
+        return term + composer;
+    }
+
+    int substract(int subtracted, int subtrahend) {
+        return subtracted - subtrahend;
+    }
+
+    int multiply(int multiplied, int multiplier) {
+        return multiplied * multiplier;
+    }
+
+    int divide(int dividend, int divider) {
+        return dividend / divider;
+    }
+
+}
