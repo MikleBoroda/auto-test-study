596be2b21b30afd36eb065a1354cac819b29c95e
MikleBoroda <mikle.kuznecov@gmail.com>
2021-06-29 01:24:19 +0300

    Создание паспорта

---
+++ src/main/java/lesson5/task5/Application.java
@@ -0,0 +1,29 @@
+package lesson5.task5;
+
+import lesson5.task5.exception.*;
+
+public class Application {
+    public static void main(String[] args) {
+        createPassport("1555", "567890");
+        createPassport("", "");
+        createPassport("12-34", "567890");
+        createPassport("12-34", "567890");
+        createPassport(null, "123456");
+        createPassport("12-34", null);
+        createPassport(null, null);
+
+    }
+
+    public static Passport createPassport(String series, String number) {
+        try {
+            return new Passport(series, number);
+        } catch (PassportSeriesArgumentException | PassportNumberArgumentException |
+                PassportSeriesNullPointerException | PassportNumberNullPointerException exception){
+            System.out.println("Введите верные значения серии и номера паспорта");
+            return null;
+        } catch (PassportAlreadyExistsException exception){
+            System.out.println("Данные серия и номер паспорта уже существуют");
+            return null;
+        }
+    }
+}
---
+++ src/main/java/lesson5/task5/exception/PassportNumberArgumentException.java
@@ -0,0 +1,4 @@
+package lesson5.task5.exception;
+
+public class PassportNumberArgumentException extends PassportIllegalArgumentException{
+}
