5aaa4de2ced11f0bef70e8c8b0a6228d45c60ded
MikleBoroda <mikle.kuznecov@gmail.com>
2021-06-29 23:22:32 +0300

    Создание Humans со всеми контактактными телефонами

---
+++ src/main/java/lesson6/task2/generators/AgeGenerator.java
@@ -0,0 +1,9 @@
+package lesson6.task2.generators;
+
+import java.util.Random;
+
+public class AgeGenerator {
+    public static int generate() {
+        return 20 + new Random().nextInt(55);
+    }
+}
