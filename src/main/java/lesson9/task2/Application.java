806782ef4eaf5d11f1e65e45dd8949df5d1e6b78
MikleBoroda <mikle.kuznecov@gmail.com>
2021-09-05 20:16:20 +0300

    Создание фигур и предметов

---
+++ src/main/java/lesson9/task2/Application.java
@@ -0,0 +1,19 @@
+package lesson9.task2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        List<Object> objectList = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            objectList.add(Generator.generate());
+        }
+
+        for (Object object : objectList
+        ) {
+            Action.action(object);
+        }
+    }
+}
---
+++ src/main/java/lesson9/task2/Circle.java
@@ -0,0 +1,8 @@
+package lesson9.task2;
+
+public class Circle implements Drawable {
+    @Override
+    public void draw() {
+        System.out.println("Фигура <Куг> нарисована");
+    }
+}
