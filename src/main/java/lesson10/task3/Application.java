a14397aa399d73c6807d741b8d5b85e53404fb1b
MikleBoroda <mikle.kuznecov@gmail.com>
2021-09-10 01:23:18 +0300

    Генерация файлов

---
+++ src/main/java/lesson10/task3/Application.java
@@ -0,0 +1,10 @@
+package lesson10.task3;
+
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        Generator generator = new FileGen(10);
+        generator.generate();
+    }
+}
